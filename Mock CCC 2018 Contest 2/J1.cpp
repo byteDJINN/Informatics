15f48e2207bcc7e757fd52a82e225f77f0abc004
Djimon Jayasundera <djimondjayasundera@icloud.com>
2019-03-06 19:36:37 +0800

    Create J1.cpp

---
+++ Mock CCC 2018 Contest 2/J1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,b,c;
+	scanf("%d + %d = %d",&a,&b,&c);
+	if (a + b == c) {
+		printf("True\n");
+	}
+	else {
+		printf("False\n");
+	}
+}
