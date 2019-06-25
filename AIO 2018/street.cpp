c4f9b23f8244b30559ba204f46464cfa3c27ff59
Djimon Jayasundera <djimondjayasundera@icloud.com>
2019-06-25 10:20:04 +0800

    Add files via upload

--- AIO 2018/street.cpp
+++ AIO 2018/street.cpp
@@ -1,19 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-    ifstream inFile;
-    ofstream outFile;
-    inFile.open("streetin.txt");
-    outFile.open("streetout.txt");
-    int n; int k;
-    inFile >> n;
-    inFile >> k;
-    int answer;
-    answer = n/(k+1); 
-    
-    outFile << answer;
-    inFile.close();
-    outFile.close();
-    cout << answer << endl;
-    return 0;
+    FILE * inp = fopen("streetin.txt","r");
+    FILE * out = fopen("streetout.txt","w");
+    int n,k;
+    fscanf(inp,"%d %d",&n,&k);
+    int answer = (int)n/(k+1);
+    fprintf(out,"%d",answer);
+    fclose(out);
+    fclose(inp);
 }
